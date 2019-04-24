af45d47d1d6a6c9ad03758036ac7f9878dd99f3e
utkarsh kandpal <utkarshkpal@gmail.com>
2019-04-24 11:42:02 +0530

    Updates

---
+++ precache-manifest.4a0a0ca6f341d54c44fc9e5307498f80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035017c8de7e63cd4dfe14487fe07994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847559dafacb212fbc40",
+    "url": "/static/css/main.ca4cfe63.chunk.css"
+  },
+  {
+    "revision": "8b2c0cf684b34785e44d",
+    "url": "/static/js/2.de8a3be8.chunk.js"
+  },
+  {
+    "revision": "847559dafacb212fbc40",
+    "url": "/static/js/main.ef7cc76a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
